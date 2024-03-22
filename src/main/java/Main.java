45425cc3958fe8d4cc8f03a732db657b06bc10c1
bronte000 <bronte000@naver.com>
2024-03-22 13:11:38 +0900

    Feat: 게임 시작, 재시작, 종료 로직 작성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("게임을 시작합니다.");
+        while (true) {
+
+            // NeedToImplement MainAlgorithm
+
+            if (!askNewGame()) {
+                break;
+            }
+        }
+    }
+
+    public static boolean askNewGame() {
+        String userAnswer;
+
+        while (true) {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+            userAnswer = scanner.nextLine();
+            if (userAnswer.equals("1")) {
+                System.out.println("게임을 새로 시작합니다.");
+                return true;
+            }
+            if (userAnswer.equals("2")) {
+                System.out.println("게임을 종료합니다.");
+                return false;
+            }
+
+            System.out.println("입력이 잘못되었습니다.");
+        }
+    }
 }
